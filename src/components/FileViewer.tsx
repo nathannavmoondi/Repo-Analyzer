b45d41bc37efc3ec511d46671f96b3ce9d8f1fae
nathannavmoondi <nav.moondi@gmail.com>
2025-06-06 12:36:36 -0400

    feat: add CircularProgress component for loading indication in FileViewer

--- src/components/FileViewer.tsx
+++ src/components/FileViewer.tsx
@@ -1,4 +1,5 @@
 import { styled } from '@mui/material/styles';
+import { Box, CircularProgress } from '@mui/material';
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
